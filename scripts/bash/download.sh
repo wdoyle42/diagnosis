12f7122e13c61940497feeb940cdc26fbe66b02f
Will Doyle <wdoyle42@gmail.com>
2018-02-05 16:42:56 -0600

    Updated download paths and file-- added loop to save code

--- scripts/bash/download.sh
+++ scripts/bash/download.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 ##Change your download directory here
-download_dir=../../acs/
+download_dir=../../data/acs/
 
 cd "$download_dir"
 
-wget http://www2.census.gov/programs-surveys/acs/data/pums/2013/3-Year/csv_hus.zip
+YEAR=2008
 
-wget http://www2.census.gov/programs-surveys/acs/data/pums/2012/3-Year/csv_hus.zip
-
-wget http://www2.census.gov/programs-surveys/acs/data/pums/2011/3-Year/csv_hus.zip
-
-wget http://www2.census.gov/programs-surveys/acs/data/pums/2010/3-Year/csv_hus.zip
-
-wget http://www2.census.gov/programs-surveys/acs/data/pums/2009/3-Year/csv_hus.zip
-
-wget http://www2.census.gov/programs-surveys/acs/data/pums/2008/3-Year/csv_hus.zip
+while [ $YEAR -lt 2017 ]; do
+  echo Downloading $YEAR . . .
+  echo wget http://www2.census.gov/programs-surveys/acs/data/pums/$YEAR/1-Year/csv_hus.zip
+  let YEAR=YEAR+1
+done
